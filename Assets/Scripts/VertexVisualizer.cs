a8fcd29c320a6a4ec95173586e9d23cf8439e0b7
Jack Yang <jackyangzzh@gmail.com>
2023-01-22 10:50:00 -0800

    terrain

--- Assets/Scripts/VertexVisualizer.cs
+++ Assets/Scripts/VertexVisualizer.cs
@@ -1,13 +1,22 @@
+using UnityEditor;
 using UnityEngine;
 
 [ExecuteInEditMode]
 public class VertexVisualizer : MonoBehaviour
 {
     [SerializeField]
-    private bool showLocal = true;
+    private Options option;
 
     private Mesh mesh = null;
     private Vector3[] vertices = null;
+
+    private enum Options
+    {
+        Local,
+        Global,
+        Viewspace,
+        None
+    }
 
     void OnDrawGizmos()
     {
@@ -17,22 +26,35 @@ public class VertexVisualizer : MonoBehaviour
             vertices = mesh.vertices;
         }
 
-        if (showLocal)
+        switch (option)
         {
-            foreach (Vector3 v in vertices)
-            {
-                string localVert = $"v: {v.ToString()}";
-                UnityEditor.Handles.Label(transform.position + v, localVert);
-            }
-        }
-        else
-        {
-            foreach (Vector3 v in vertices)
-            {
-                string worldVert = $"wv: {(transform.position + v).ToString()}";
-                UnityEditor.Handles.Label(transform.position + v, worldVert);
-            }
-        }
+            case Options.Local:
+                foreach (Vector3 v in vertices)
+                {
+                    string localVert = $"v: {v.ToString()}";
+                    UnityEditor.Handles.Label(transform.position + v, localVert);
+                }
+                break;
 
+            case Options.Global:
+                foreach (Vector3 v in vertices)
+                {
+                    string worldVert = $"wv: {(transform.position + v).ToString()}";
+                    UnityEditor.Handles.Label(transform.position + v, worldVert);
+                }
+                break;
+
+            case Options.Viewspace:
+                foreach (Vector3 v in vertices)
+                {
+                    Vector3 viewVector = SceneView.GetAllSceneCameras()[0].WorldToViewportPoint(transform.position + v);
+                    string viewVert = $"vv: {viewVector.ToString()}";
+                    UnityEditor.Handles.Label(transform.position + v, viewVert);
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
